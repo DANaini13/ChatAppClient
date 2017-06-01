642ab014817f3b65d915e3f0cfb9d308672fd0a3
Zeyong Shan <shan27@pdx.edu>
2017-05-31 17:50:58 -0700

    Update Main.java

--- com/nasoftware/Main.java
+++ com/nasoftware/Main.java
@@ -6,12 +6,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 	// write your code here
-    //example code for using NAChatClient:
-        NAManager.init();
-        Holder holder = (String result) ->
-        {
-            NAManager.messagesManager.addMessageListener((String a) -> System.out.println(a));
-        };
-        NAManager.accountManager.logIn("shan27", "BIANhao5213", holder);
+    	// Testing client here
     }
 }
